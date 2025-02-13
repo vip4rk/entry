eb4bf1e8559e003de3a56086d77ea29b41882a44
Victor Martin <victor.minerve@gmail.com>
2025-02-13 08:25:32 +0100

    test

--- db_connect.php
+++ db_connect.php
@@ -14,7 +14,11 @@ if ($conn->connect_error) {
 } else {
     echo "Connexion réussie à la base de données !";  // Pour vérifier si la connexion fonctionne
 }
-//git config git-ftp.url ftp://rklg4755:O2switch1603\*@nirrti.o2switch.net:21/public_html/entry
-//git ftp init -v --user rklg4755 --passwd O2switch1603\* ftp://nirrti.o2switch.net:21/public_html/entry
+//git config git-ftp.url ftp://rklg4755:O2switch1603@nirrti.o2switch.net:21/public_html/entry
+//git ftp init -v --user rklg4755 --passwd O2switch1603 ftp://nirrti.o2switch.net:21/public_html/entry
+//git config git-ftp.user rklg4755
+//git config git-ftp.url ftp.rklg4755.odns.fr
+//git config git-ftp.password O2switch1603
+//git config git-ftp.syncroot /home/rklg4755/public_html/entry
 
 ?>
